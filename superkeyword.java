cc7fa7de83e3834ca9546c2fbbe7386ba5dbd70e
sunilreddy160 <sunilrampatruni@gmail.com>
2024-02-25 08:56:19 -0500

    one file added

---
+++ superkeyword.java
@@ -0,0 +1,25 @@
+package corejava;
+    class A {
+    	String name = "sunil";
+    	void display() {
+    		System.out.println("this super class");
+    	}
+    }
+    class B extends A {
+    	String name = "reddy";
+    	void display() {
+     System.out.println(name);
+    	System.out.println(super.name);
+    	}
+    	void print() {
+    		super.display();
+    	}
+    }
+public class superkeyword  {
+    public static void main(String[] args) {
+                B b = new B();   
+         b.display();   
+         b.print();
+    }
+
+}
